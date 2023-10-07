3c1edc0a02eca64040fa272005648525e7ebd059
Scrimali Anthony <anthony.scrimali@gmail.com>
2023-10-07 20:21:19 +0200

    Python3

---
+++ Power of Thor - Episode 1/power_of_thor_episode-1.py
@@ -0,0 +1,25 @@
+import sys
+import math
+
+light_x, light_y, initial_tx, initial_ty = [int(i) for i in input().split()]
+
+while True:
+    remaining_turns = int(input())
+
+    d = ""
+
+    if initial_ty > light_y:
+        initial_ty -= 1
+        d += "N"
+    elif initial_ty < light_y:
+        initial_ty += 1
+        d += "S"
+
+    if initial_tx > light_x:
+        initial_tx += 1
+        d += "W"
+    elif initial_tx < light_x:
+        initial_tx -= 1
+        d += "E"
+    
+    print(d)
